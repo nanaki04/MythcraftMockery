10608f1789695579742048a3f5b27da88e52aaac
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-05-06 23:22:48 +0900

    added the show_error method to the rendererBridge

--- lib/client/rendererBridge/rendererBridge.ex
+++ lib/client/rendererBridge/rendererBridge.ex
@@ -1,4 +1,5 @@
 defmodule Mc.Mock.RendererBridge do
-  def show_menu(_renderer_id, %{}), do: :ok
-  def pop_menu(_renderer_id), do: :ok
+  def show_menu(_client_id, %{}), do: :ok
+  def pop_menu(_client_id), do: :ok
+  def show_error(_client_id, error_message), do: error_message
 end
